01974ff0ad6746dff5820b4fca79cc0f227af663
Arvi-H <arvihaxhillari1@gmail.com>
2022-03-12 11:08:19 -0700

    Created Maze Class

---
+++ Maze.h
@@ -0,0 +1,19 @@
+#include "MazeInterface.h"
+
+class Maze : public MazeInterface {
+public:
+    Maze();
+    ~Maze();
+
+    void setValue(int height, int width, int layer, int value) {
+        
+    }
+    
+    bool find_maze_path() {
+
+    }
+
+    std::string toString() const {
+
+    }
+};
