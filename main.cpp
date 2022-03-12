ea7ae80bd2c3a038634c1bde25a2a82810928d40
Arvi-H <arvihaxhillari1@gmail.com>
2022-03-12 15:28:22 -0700

    Now initalizing correctly lol

--- main.cpp
+++ main.cpp
@@ -6,25 +6,41 @@
 using namespace std;
 
 /**
- * Print our 3D maze, replacing 0s with "_" and 1s with "X".
+ * Initalize 3D maze, turning 0s to "_" and 1s to "X".
 */
- 
-
 void initMaze(Maze maze, int layers, int height, int width, ifstream &in) {
     int input;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            for (int k = 0; k < layers; k++) {
+                in >> input;
+                
+                if (input == 0) {
+                    maze.setValue(i, j, k, 0);
+                } else if (input == 1) {
+                    maze.setValue(i, j, k, 1);
+                }
+            }
+        }
+    } 
+}
+
+/**
+ * Print 3D maze in a nice format.
+*/
+void printMaze(string mazeLayout, int height, int width, int layers, ofstream &out) {
+    int count = 0;
 
     for (int i = 0; i < layers; i++) {
         for (int j = 0; j < height; j++) {
             for (int k = 0; k < width; k++) {
-                in >> input;
-                
-                if (input == 0) {
-                    maze.setValue(j, k, i, 0);
-                } else if (input == 1) {
-                    maze.setValue(j, k, i, 1);
-                }
+                out << mazeLayout[count] << " ";
+                count++;
             }
-        }
+            out << endl;
+        }   
+        out << endl;
     }
 }
 
@@ -48,8 +64,11 @@ int main(int argc, char* argv[]) {
 
     cout << mazeLayout << endl;
     initMaze(maze, layers, height, width, in);
+    cout << mazeLayout << endl;
     
     mazeLayout = maze.toString();
     cout << mazeLayout << endl;
+
+    printMaze(mazeLayout, height, width, layers, out);
 
 }   
